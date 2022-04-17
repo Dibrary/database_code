d1836f1d0b0619217b0511bb5f177076b5924e42
Dibrary <kkdh828@hanmail.net>
2022-04-17 19:10:53 +0900

    220417 mysql - insert / select 코드

--- mysql/insert_code.py
+++ mysql/insert_code.py
@@ -1,2 +1,11 @@
 import pymysql
 
+connector = pymysql.connect(host='localhost', port=3306, db='practice', user='root', password='비밀번호', charset='utf8')
+cursor = connector.cursor()
+
+sql = 'INSERT INTO book(id, divided_no, bookname, writer, publisher) values(%s,%s,%s,%s,%s)'
+cursor.execute(sql, ('1','325.04','플렛폼제국의 성공 시나리오','다나카 미치아키','이너북'))
+
+connector.commit()
+
+print("데이터 삽입 성공")
--- mysql/select_code.py
+++ mysql/select_code.py
@@ -1,2 +1,11 @@
 import pymysql
 
+
+connector = pymysql.connect(host='localhost', port=3306, db='practice', user='root', password='비밀번호', charset='utf8')
+cursor = connector.cursor()
+
+sql = 'SELECT * from book'
+cursor.execute(sql)
+values = cursor.fetchall()
+
+print(values, len(values), type(values))
